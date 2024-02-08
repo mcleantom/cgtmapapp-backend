cff75160054ec6790a504f49ada8a48dfd242be8
Tom McLean <64887735+mcleantom@users.noreply.github.com>
2024-02-08 20:06:56 +0000

    allow all origins

--- cgt_map_backend/app.py
+++ cgt_map_backend/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .companies_router import create_companies_router
 from .config import CGTMapBackendConfig
@@ -11,6 +12,14 @@ def create_app(config: CGTMapBackendConfig) -> FastAPI:
 
     app = FastAPI(
         title=config.title,
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     companies_router = create_companies_router()
